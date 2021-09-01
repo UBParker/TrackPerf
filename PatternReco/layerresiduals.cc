fdb7fa994176b616682971cfe1b46cbd5269d48b
Ashley Marie Parker <ash.marie.parker@gmail.com>
2021-09-01 10:36:21 +0200

    add displaced seeds and L2L3 seeds to layer residuals plots

--- PatternReco/layerresiduals.cc
+++ PatternReco/layerresiduals.cc
@@ -16,10 +16,10 @@ public:
   PlotResiduals(int layer, int seedindex){
     layer_=layer;
     seedindex_=seedindex;
-    double drphimax=0.2;
-    double dzmax=15.0;
+    double drphimax=0.1;//0.2
+    double dzmax=5.0;//15
     if ((layer_>4)&&(seedindex_==1)) drphimax=1.0;
-    if (seedindex_==1) dzmax=10.0;
+    if (seedindex_==1) dzmax=5.0; //10
     string name="r*phi residual in layer "+std::to_string(layer)+", "+seedname(seedindex)
       +" seed, pt<3 GeV";
     hist16l_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
@@ -48,12 +48,17 @@ public:
   string seedname(int seedindex) {
 
     if (seedindex==0) return "L1L2";
-    if (seedindex==1) return "L3L4";
-    if (seedindex==2) return "L5L6";
-    if (seedindex==3) return "D1D2";
-    if (seedindex==4) return "D3D4";
-    if (seedindex==5) return "L1D1";
-    if (seedindex==6) return "L2D1";
+    if (seedindex==1) return "L2L3";
+    if (seedindex==2) return "L3L4";
+    if (seedindex==3) return "L5L6";
+    if (seedindex==4) return "D1D2";
+    if (seedindex==5) return "D3D4";
+    if (seedindex==6) return "L1D1";
+    if (seedindex==7) return "L2D1";
+    if (seedindex==8) return "L3L4L2";
+    if (seedindex==9) return "L5L6L4";
+    if (seedindex==10) return "L2L3D1";
+    if (seedindex==11) return "D1D2L2";   
 
     return "Unkown seedindex";
     
@@ -91,22 +96,43 @@ public:
   void Draw(TCanvas* c){
 
     cout << "layer seed phicut : "<<layer_<<" "<<seedindex_<<" "<<dphicut_<<endl;
-    
+
+    TLegend* leg3 = new TLegend(0.37,0.64,0.59,0.85);
+    leg3->SetTextSize(0.08);
+    leg3->SetFillColor(0);
+    leg3->SetBorderSize(0);
+
+
     c->cd(1);
     hist16l_->SetLineColor(kBlue);
-    hist16l_->Draw();
+
+    hist116l_->GetXaxis()->SetNdivisions(6);
     hist116l_->SetLineColor(kRed);
-    hist116l_->Draw("Same");
+    hist116l_->Draw();
+    hist116l_->SetMaximum(2.61 *hist16l_->GetMaximum() );
+    hist16l_->Draw("Same");
+
+
     TLine* ll1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116l_->GetMaximum());
     ll1->Draw();
     TLine* ll2 = new TLine(dphicut_,0,dphicut_,0.5*hist116l_->GetMaximum());
     ll2->Draw();
 
+    leg3->AddEntry( hist16l_, "float", "L" );
+    leg3->AddEntry( hist116l_, "int", "L");
+    leg3->Draw();
+    c->Update();
+
+
     c->cd(2);
     hist16m_->SetLineColor(kBlue);
-    hist16m_->Draw();
+
+    hist116m_->GetXaxis()->SetNdivisions(6);
     hist116m_->SetLineColor(kRed);
-    hist116m_->Draw("Same");
+    hist116m_->Draw();
+    hist116m_->SetMaximum(1.61 *hist116m_->GetMaximum() );
+    hist16m_->Draw("Same");
+
     TLine* lm1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116m_->GetMaximum());
     lm1->Draw();
     TLine* lm2 = new TLine(dphicut_,0,dphicut_,0.5*hist116m_->GetMaximum());
@@ -114,9 +140,14 @@ public:
 
     c->cd(3);
     hist16h_->SetLineColor(kBlue);
-    hist16h_->Draw();
+
+
+    hist116h_->GetXaxis()->SetNdivisions(6);
     hist116h_->SetLineColor(kRed);
-    hist116h_->Draw("Same");
+    hist116h_->Draw();
+    hist116h_->SetMaximum(1.61 *hist116h_->GetMaximum() );
+    hist16h_->Draw("Same");
+
     TLine* lh1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116h_->GetMaximum());
     lh1->Draw();
     TLine* lh2 = new TLine(dphicut_,0,dphicut_,0.5*hist116h_->GetMaximum());
@@ -124,13 +155,15 @@ public:
 
     c->cd(4);
     hist16_->SetLineColor(kBlue);
-    hist16_->Draw();
+    hist116_->GetXaxis()->SetNdivisions(15);
     hist116_->SetLineColor(kRed);
-    hist116_->Draw("Same");
-    TLine* l1 = new TLine(-dzcut_,0,-dzcut_,0.5*hist116_->GetMaximum());
-    l1->Draw();
-    TLine* l2 = new TLine(dzcut_,0,dzcut_,0.5*hist116_->GetMaximum());
-    l2->Draw();
+    hist116_->Draw();
+    hist116_->SetMaximum(1.61 *hist116_->GetMaximum() );
+    hist16_->Draw("Same");
+    //TLine* l1 = new TLine(-drcut_,0,-drcut_,0.5*hist116_->GetMaximum());
+    //l1->Draw();
+    //TLine* l2 = new TLine(drcut_,0,drcut_,0.5*hist116_->GetMaximum());
+    //l2->Draw();
 
   }
   
@@ -141,6 +174,8 @@ private:
   double dphicut_;
   double dzcut_;
   
+  TLegend *leg3;
+
   TH1 *hist16l_;
   TH1 *hist116l_;
   TH1 *hist16m_;
@@ -194,6 +229,38 @@ gStyle->SetOptTitle(1);
  c1->SetGrid();
 
 
+//    if (seedindex==0) return "L1L2";
+//    if (seedindex==1) return "L2L3";
+//    if (seedindex==2) return "L3L4";
+ //   if (seedindex==3) return "L5L6";
+ //   if (seedindex==4) return "D1D2";
+ ///   if (seedindex==5) return "D3D4";
+ //   if (seedindex==6) return "L1D1";
+ //   if (seedindex==7) return "L2D1";
+ //   if (seedindex==8) return "L3L4L2";
+  //  if (seedindex==9) return "L5L6L4";
+   // if (seedindex==10) return "L2L3D1";
+    //if (seedindex==11) return "D1D2L2"; 
+
+
+//  PlotResiduals (layer, seed index)      
+
+ PlotResiduals Resid_L3_L5L6L4(3,9);
+ PlotResiduals Resid_L3_D1D2L2(3,11);
+
+ PlotResiduals Resid_L2_L5L6L4(2,9);
+
+ PlotResiduals Resid_L1_L3L4L2(1,8);
+ PlotResiduals Resid_L1_L5L6L4(1,9);
+ PlotResiduals Resid_L1_L2L3D1(1,10); 
+ PlotResiduals Resid_L1_D1D2L2(1,11);
+
+ PlotResiduals Resid_L4_L2L3D1(4,10); 
+ PlotResiduals Resid_L4_D1D2L2(4,11);
+
+ PlotResiduals Resid_L6_L3L4L2(6,8);
+
+
  PlotResiduals Resid_L3_L1L2(3,0);
  PlotResiduals Resid_L4_L1L2(4,0);
  PlotResiduals Resid_L5_L1L2(5,0);
@@ -204,6 +271,8 @@ gStyle->SetOptTitle(1);
  PlotResiduals Resid_L5_L3L4(5,1);
  PlotResiduals Resid_L6_L3L4(6,1);
 
+
+
  PlotResiduals Resid_L1_L5L6(1,2);
  PlotResiduals Resid_L2_L5L6(2,2);
  PlotResiduals Resid_L3_L5L6(3,2);
@@ -211,12 +280,12 @@ gStyle->SetOptTitle(1);
 
  PlotResiduals Resid_L1_D1D2(1,3);
  PlotResiduals Resid_L1_D3D4(1,4);
- PlotResiduals Resid_L1_L2D1(1,6);
+ PlotResiduals Resid_L1_L2D1(1,7);
  
  PlotResiduals Resid_L2_D1D2(2,3);
 
 
- ifstream in("layerresiduals.txt");
+ ifstream in("layerresiduals_prompt_all.txt"); //#prompt / disp
 
  int count=0;
 
@@ -249,7 +318,21 @@ gStyle->SetOptTitle(1);
  
    Resid_L2_D1D2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
 
-   
+   Resid_L3_L5L6L4.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L3_D1D2L2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L2_L5L6L4.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L1_L3L4L2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L1_L5L6L4.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L1_L2L3D1.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut); 
+   Resid_L1_D1D2L2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+
+   Resid_L4_L2L3D1.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut); 
+   Resid_L4_D1D2L2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+   Resid_L6_L3L4L2.addResid(layer, seedindex, pt, idphi, dphi, dphicut, idz, dz, dzcut);
+
+
+
+
    count++;
 
  }
@@ -257,39 +340,68 @@ gStyle->SetOptTitle(1);
 //cout << "Processed: "<<count<<" events"<<endl;
 
  Resid_L1_L3L4.Draw(c1);
- c1->Print("layerresiduals.pdf(","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf(","pdf");
  Resid_L1_L5L6.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L1_D1D2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L1_D3D4.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L1_L2D1.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L2_L3L4.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L2_L5L6.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L2_D1D2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L3_L1L2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L3_L5L6.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L4_L1L2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L4_L5L6.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L5_L1L2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L5_L3L4.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L6_L1L2.Draw(c1);
- c1->Print("layerresiduals.pdf","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf","pdf");
  Resid_L6_L3L4.Draw(c1);
- c1->Print("layerresiduals.pdf)","pdf");
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
 
- 
+ Resid_L3_L5L6L4.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L3_D1D2L2.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L2_L5L6L4.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+
+ Resid_L1_L3L4L2.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+ Resid_L1_L5L6L4.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L1_L2L3D1.Draw(c1); 
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L1_D1D2L2.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L4_L2L3D1.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L4_D1D2L2.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
+ Resid_L6_L3L4L2.Draw(c1);
+ c1->Print("layerresiduals_ALLprompt.pdf)","pdf");
+
 
 
 
