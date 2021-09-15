b04227083fc59372762c1288eb4bb0fbfac72219
Ashley Marie Parker <ash.marie.parker@gmail.com>
2021-09-15 14:32:25 +0200

    Update diskresiduals.cc

--- PatternReco/diskresiduals.cc
+++ PatternReco/diskresiduals.cc
@@ -4,6 +4,7 @@
 #include "TStyle.h"
 #include "TLorentzVector.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 #include "TH1.h"
 #include "TGaxis.h"
 #include <fstream>
@@ -17,26 +18,29 @@ public:
     disk_=disk;
     isPS_=isPS;
     seedindex_=seedindex;
-    double drphimax=1.0;
-    double drmax=10.0;
-    if (isPS==1) drmax=5.0;
+    double drphimax=2. ;//0.50;// was 0.1
+    double drmax=10.0; // was 10
+    if (isPS==1) drmax=5.0; //was 5
+    int nBinsdrphi = 25;
+    int nBinsdr = 10;
+
     string name="r*phi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, pt<3 GeV";
-    hist16l_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist16l_ = new TH1F(name.c_str(),name.c_str(),nBinsdrphi ,-drphimax,drphimax);
     name="r*iphi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, pt<3 GeV";
-    hist116l_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist116l_ = new TH1F(name.c_str(),name.c_str(), nBinsdrphi ,-drphimax,drphimax);
     name="r*phi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, 3<pt<8 GeV";
-    hist16m_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist16m_ = new TH1F(name.c_str(),name.c_str(), nBinsdrphi ,-drphimax,drphimax);
     name="r*iphi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, 3<pt<8 GeV";
-    hist116m_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist116m_ = new TH1F(name.c_str(),name.c_str(), nBinsdrphi ,-drphimax,drphimax);
     name="r*phi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, pt>8 GeV";
-    hist16h_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist16h_ = new TH1F(name.c_str(),name.c_str(), nBinsdrphi ,-drphimax,drphimax);
     name="r*iphi residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex)+" seed, pt>8 GeV";
-    hist116h_ = new TH1F(name.c_str(),name.c_str(),50,-drphimax,drphimax);
+    hist116h_ = new TH1F(name.c_str(),name.c_str(), nBinsdrphi ,-drphimax,drphimax);
     
     name="r residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex);
-    hist16_ = new TH1F(name.c_str(),name.c_str(),40,-drmax,drmax);
+    hist16_ = new TH1F(name.c_str(),name.c_str(), nBinsdr ,-drmax,drmax);
     name="ir residual in D"+std::to_string(disk)+ps2s(isPS)+", "+seedname(seedindex);
-    hist116_ = new TH1F(name.c_str(),name.c_str(),40,-drmax,drmax);
+    hist116_ = new TH1F(name.c_str(),name.c_str(), nBinsdr ,-drmax,drmax);
 }
 
   int addResid(int disk, int isPS, int seedindex, double pt, double idphi, double dphi, double dphicut, double idr, double dr, double drcut){
@@ -74,21 +78,43 @@ public:
 
     cout << "disk seedindex phicut : "<<disk_<<" "<<seedindex_<<" "<<dphicut_<<endl;
     
+
+    TLegend* leg3 = new TLegend(0.37,0.64,0.59,0.85);
+    leg3->SetTextSize(0.08);
+    leg3->SetFillColor(0);
+    leg3->SetBorderSize(0);
+
+
     c->cd(1);
     hist16l_->SetLineColor(kBlue);
-    hist16l_->Draw();
+
+    hist116l_->GetXaxis()->SetNdivisions(12);
     hist116l_->SetLineColor(kRed);
-    hist116l_->Draw("Same");
+    hist116l_->Draw();
+    hist116l_->SetMaximum(2.61 *hist16l_->GetMaximum() );
+    hist16l_->Draw("Same");
+
+
     TLine* ll1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116l_->GetMaximum());
     ll1->Draw();
     TLine* ll2 = new TLine(dphicut_,0,dphicut_,0.5*hist116l_->GetMaximum());
     ll2->Draw();
 
+    leg3->AddEntry( hist16l_, "float", "L" );
+    leg3->AddEntry( hist116l_, "int", "L");
+    leg3->Draw();
+    c->Update();
+
+
     c->cd(2);
     hist16m_->SetLineColor(kBlue);
-    hist16m_->Draw();
+
+    hist116m_->GetXaxis()->SetNdivisions(12);
     hist116m_->SetLineColor(kRed);
-    hist116m_->Draw("Same");
+    hist116m_->Draw();
+    hist116m_->SetMaximum(1.61 *hist116m_->GetMaximum() );
+    hist16m_->Draw("Same");
+
     TLine* lm1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116m_->GetMaximum());
     lm1->Draw();
     TLine* lm2 = new TLine(dphicut_,0,dphicut_,0.5*hist116m_->GetMaximum());
@@ -96,9 +122,14 @@ public:
 
     c->cd(3);
     hist16h_->SetLineColor(kBlue);
-    hist16h_->Draw();
+
+
+    hist116h_->GetXaxis()->SetNdivisions(12);
     hist116h_->SetLineColor(kRed);
-    hist116h_->Draw("Same");
+    hist116h_->Draw();
+    hist116h_->SetMaximum(1.61 *hist116h_->GetMaximum() );
+    hist16h_->Draw("Same");
+
     TLine* lh1 = new TLine(-dphicut_,0,-dphicut_,0.5*hist116h_->GetMaximum());
     lh1->Draw();
     TLine* lh2 = new TLine(dphicut_,0,dphicut_,0.5*hist116h_->GetMaximum());
@@ -106,9 +137,11 @@ public:
 
     c->cd(4);
     hist16_->SetLineColor(kBlue);
-    hist16_->Draw();
+    hist116_->GetXaxis()->SetNdivisions(12);
     hist116_->SetLineColor(kRed);
-    hist116_->Draw("Same");
+    hist116_->Draw();
+    hist116_->SetMaximum(1.61 *hist116_->GetMaximum() );
+    hist16_->Draw("Same");
     TLine* l1 = new TLine(-drcut_,0,-drcut_,0.5*hist116_->GetMaximum());
     l1->Draw();
     TLine* l2 = new TLine(drcut_,0,drcut_,0.5*hist116_->GetMaximum());
@@ -150,6 +183,9 @@ private:
   double dphicut_;
   double drcut_;
   
+
+  TLegend *leg3;
+
   TH1 *hist16l_;
   TH1 *hist116l_;
   TH1 *hist16m_;
@@ -163,7 +199,7 @@ private:
 };
 
 
-void diskresiduals_displaced(){
+void diskresiduals(){
 //
 // To see the output of this macro, click here.
 
@@ -198,9 +234,12 @@ gStyle->SetOptTitle(1);
 
 
  TCanvas* c1 = new TCanvas("c1","Track performance",200,10,700,800);
+ c1->SetTickx(10);//NEW ???
  c1->Divide(2,2);
  c1->SetFillColor(0);
  c1->SetGrid();
+
+
 
  PlotResiduals Resid_D1PS_L1L2(1,1,0);
  PlotResiduals Resid_D12S_L1L2(1,0,0);
@@ -266,18 +305,14 @@ gStyle->SetOptTitle(1);
 
  while (in.good()) {
 
-   double disk,isPS,pt,idphi,dphi,dphicut,idr,dr,drcut;
+   double disk,isPS,pt,idphi,dphi,dphicut,idr,dr,drcut;//#, rinv;
    int seedindex=-1;
-   in>>disk>>isPS>>seedindex>>pt>>idphi>>dphi>>dphicut>>idr>>dr>>drcut;
+
+   in>>disk>>isPS>>seedindex>>pt>>idphi>>dphi>>dphicut>>idr>>dr>>drcut;//>>rinv;
 
    if (!in.good()) continue;
    
 
-
-   // if (seedindex==8) return "L3L4L2";
-   // if (seedindex==9) return "L5L6L4";
-   // if (seedindex==10) return "L2L3D1";
-   // if (seedindex==11) return "D1D2L2"; 
    int added=0;
    
    added+=Resid_D1PS_L1L2.addResid(disk, isPS, seedindex, pt, idphi, dphi, dphicut, idr, dr, drcut);
@@ -332,69 +367,69 @@ gStyle->SetOptTitle(1);
 //cout << "Processed: "<<count<<" events"<<endl;
 
  Resid_D1PS_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf(","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D12S_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf(","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D12S_L3L4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D1PS_D3D4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  
  Resid_D2PS_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D22S_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D22S_L3L4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D2PS_D3D4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D2PS_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D2PS_L2D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D22S_L2D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  
  Resid_D3PS_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D32S_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D3PS_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D32S_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D3PS_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D3PS_L2D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D32S_L2D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  
  Resid_D42S_L1L2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D4PS_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D42S_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D4PS_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D42S_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D42S_L2D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf"); 
+ c1->Print("diskresiduals.pdf","pdf"); 
  
  Resid_D5PS_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D52S_D1D2.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D5PS_D3D4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D52S_D3D4.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D5PS_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
  Resid_D52S_L1D1.Draw(c1);
- c1->Print("diskresiduals_disp.pdf)","pdf");
+ c1->Print("diskresiduals.pdf","pdf");
 
 }
 
